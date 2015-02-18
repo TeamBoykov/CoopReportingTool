92bf2f85d10d2342a105b9d2a828a4df68a67423
srichatala <sri2vas@gmail.com>
2015-02-18 02:01:45 -0500

    Master page template and design, login page and design, and class files for login are created

---
+++ CoopReportingApp/App_Code/DAL/LoginDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for LoginDAL
+/// </summary>
+public class LoginDAL
+{
+	public LoginDAL()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
