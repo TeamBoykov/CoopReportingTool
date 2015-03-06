e6c2afd934988faff392a43fbd5597a671a73317
subash433 <subash433@gmail.com>
2015-03-06 17:17:17 -0500

    HTML Grading Page Created #17

---
+++ CoopReportingApp/CoopAdvisor/GradePage.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CoopReportingApp.CoopAdvisor
+{
+    public partial class GradePage1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lbl_msg.Visible = false;
+            DevAlertMessage.Visible = false;
+        }
+
+        protected void BtnInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection cnn = new SqlConnection("Data Source=. ; Database=coop; Integrated Security=true");
+
+        }
+
+        protected void BtnCancel_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void BunAlertMessage_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
