a41cc2d1dfc49b1514a53fc3167bbd582d0cff25
subash433 <subash433@gmail.com>
2015-03-26 22:54:36 -0400

    RegistrationBO.cs and RegistrationDAL.cs added Task #93

---
+++ CoopReportingApp/App_Code/DAL/RegistrationDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for RegistrationDAL
+/// </summary>
+public class RegistrationDAL
+{
+	public RegistrationDAL()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    // code will be added- subash
+}
